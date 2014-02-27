9dc3c4ab3e64be17954e8cccb70dd8074864e3bd
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-27 22:43:13 +0800

    Updated addBigNumber binding procedure.

--- addBigNumber.java
+++ addBigNumber.java
@@ -2,8 +2,8 @@ import java.math.BigInteger;
 public class addBigNumber{
 	public static void main( String... numbers ){
 		try{
-			BigInteger number = BigInteger.ZERO;
-			for( int index = 0; index < numbers.length; index++ ){
+			BigInteger number = new BigInteger( numbers[ 0 ] );
+			for( int index = 1; index < numbers.length; index++ ){
 				number = number.add( new BigInteger( numbers[ index ] ) );	
 			}
 			System.out.print( number.toString( ) );
